4b0cd08601a8fbc4ac52f8f61f537590de0bd967
Nina Buscemi <NinaBuscemi@ninas-mbp.lan>
2023-12-17 03:15:09 -0500

    Added changes to code

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
-// Modify the startQuiz() function as follows
 document.addEventListener("DOMContentLoaded", function () {
 
-var questions = [
-  {
-    question: "Question 1: Which HTML tag is used to define an unordered list?",
-    choices: ["<ul>", "<ol>", "<li>", "<div>"],
-    answer: 1
-  },
-  {
-    question: "Question 2: Which CSS property is used to change the color of text?",
-    choices: ["font-size", "background-color", "color", "text-align"],
-    answer: 3
-  },
-  {
-    question: "Question 3: What is the correct syntax for creating a JavaScript function?",
-    choices: ["function = myFunction() { }", "function myFunction() { }", "myFunction() = function { }", "myFunction() { }"],
-    answer: 2
-  },
-  {
-    question: "Question 4: Which JavaScript method is used to add an element to the end of an array?",
-    choices: ["push()", "pop()", "shift()", "unshift()"],
-    answer: 1
-  },
-  {
-    question: "Question 5: Which CSS property is used to change the color of text?",
-    choices: ["DOM API", "Fetch API", "Storage API", "Animation API"],
-    answer: 2
-  },
-];
+  var questions = [
+    {
+      question: "Question 1: Which HTML tag is used to define an unordered list?",
+      choices: ["<ul>", "<ol>", "<li>", "<div>"],
+      answer: 1
+    },
+    {
+      question: "Question 2: Which CSS property is used to change the color of text?",
+      choices: ["font-size", "background-color", "color", "text-align"],
+      answer: 3
+    },
+    {
+      question: "Question 3: What is the correct syntax for creating a JavaScript function?",
+      choices: [
+        "function = myFunction() { }",
+        "function myFunction() { }",
+        "myFunction() = function { }",
+        "myFunction() { }"
+      ],
+      answer: 2
+    },
+    {
+      question: "Question 4: Which JavaScript method is used to add an element to the end of an array?",
+      choices: ["push()", "pop()", "shift()", "unshift()"],
+      answer: 1
+    },
+    {
+      question: "Question 5: Which CSS property is used to change the color of text?",
+      choices: ["DOM API", "Fetch API", "Storage API", "Animation API"],
+      answer: 2
+    }
+  ];
 
-var currentQuestionIndex = 0;
+  var currentQuestionIndex = 0;
   var score = 0;
+  var timeLeft;
 
   var questionElement = document.getElementById("question");
   var choicesElement = document.getElementById("choices");
   var resultContainer = document.getElementById("result-container");
-  var resultElement = document.getElementById("result");
-  var nextButton = document.getElementById("next-btn");
-  var scoreContainer = document.getElementById("score-container");
+  var showNextButton = document.getElementById("next-btn");
   var scoreElement = document.getElementById("score");
   var restartButton = document.getElementById("restart-btn");
   var feedbackElement = document.getElementById("feedback");
   var questionContainer = document.getElementById("question-container");
+  var isNextButtonListenerAdded = false;
 
   function showQuestion() {
     var currentQuestion = questions[currentQuestionIndex];
-
-    console.log("currentQuestion:", currentQuestion);
-    console.log("questionElement:", questionElement);
-    console.log("choicesElement:", choicesElement);
-    console.log("feedbackElement:", feedbackElement);
 
     if (!currentQuestion || !questionElement || !choicesElement || !feedbackElement) {
       console.error("Error: Could not find one or more necessary elements.");
       return;
     }
 
-    // Update question text
     questionElement.textContent = currentQuestion.question;
-
-    // Clear previous choices
     choicesElement.innerHTML = "";
 
-    // Update choices
     currentQuestion.choices.forEach(function (choice, index) {
       var li = document.createElement("li");
       li.textContent = choice;
@@ -72,260 +67,209 @@ var currentQuestionIndex = 0;
       choicesElement.appendChild(li);
     });
 
-    // Show the question container
     questionContainer.style.display = "block";
   }
 
-var startButton = document.getElementById("start-btn");
-startButton.addEventListener("click", startQuiz);
+  var startButton = document.getElementById("start-btn");
+  startButton.addEventListener("click", startQuiz);
 
-var timerElement = document.getElementById("timer");
-var timerInterval;
-var timeLeft;
+  var timerElement = document.getElementById("timer");
+  var timerInterval;
 
-function startTimer() {
-  timeLeft = 75;
-  timerInterval = setInterval(function() {
-    timeLeft--;
-    timerElement.textContent = "Time: " + timeLeft + "s";
+  function startTimer() {
+    timeLeft = 75;
+    timerInterval = setInterval(function () {
+      timeLeft--;
+      timerElement.textContent = "Time: " + timeLeft + "s";
 
-    if (timeLeft <= 0) {
-      clearInterval(timerInterval);
+      if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        showScore();
+      }
+    }, 1000);
+  }
+
+  function stopTimer() {
+    clearInterval(timerInterval);
+  }
+
+  function showScore() {
+    stopTimer();
+    scoreElement.textContent = "Your score: " + score + "/" + questions.length;
+    resultContainer.style.display = "block";
+  }
+
+  function checkAnswer(choiceIndex) {
+    var currentQuestion = questions[currentQuestionIndex];
+    if (choiceIndex === currentQuestion.answer) {
+      feedbackElement.textContent = "Correct!";
+      score++;
+    } else {
+      feedbackElement.textContent = "Incorrect!";
+      timeLeft -= 10; // Deduct 10 seconds for an incorrect answer
+  }
+}
+var showNextButton = document.createElement("button");
+showNextButton.textContent = "Next";
+showNextButton.id = "next-btn";
+showNextButton.className = "next-btn";
+showNextButton.style.display = "none";
+
+questionContainer.appendChild(showNextButton);
+
+function showNextButton() {
+  showNextButton.style.display = "block";
+  if (!isNextButtonListenerAdded) {
+    showNextButton.addEventListener("click", showNextQuestion);
+    isNextButtonListenerAdded = true;
+  }
+}
+
+  function hideQuestionContainer() {
+    questionContainer.style.display = "none";
+  }
+
+  function showNextQuestion() {
+    hideResultContainer(); // Hide the result container
+
+    if (currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++;
+      showQuestion();
+      hideNextButton(); // Hide the next button when showing the next question
+    } else {
       showScore();
     }
-  }, 1000);
-}
-
-function stopTimer() {
-  clearInterval(timerInterval);
-}
-
-function showScore() {
-  stopTimer();
-}
-
-function checkAnswer(choiceIndex) {
-  var currentQuestion = questions[currentQuestionIndex];
-  if (choiceIndex === currentQuestion.answer) {
-    feedbackElement.textContent = "Correct!";
-    score++;
-  } else {
-    feedbackElement.textContent = "Incorrect!";
   }
 
-  questionContainer.style.display = "none";
-  resultContainer.style.display = "block";
-}
-
-function applyAnswerStyles(li, choiceIndex) {
-  var currentQuestion = questions[currentQuestionIndex];
-
-  // Remove existing styles
-  li.classList.remove("correct-answer", "incorrect-answer");
-
-  if (choiceIndex === currentQuestion.answer) {
-    li.classList.add("correct-answer");
-  } else {
-    li.classList.add("incorrect-answer");
-  }
-}
-
-function showNextQuestion() {
-  if (currentQuestionIndex < questions.length - 1) {
-    currentQuestionIndex++;
-    showQuestion();
-    questionContainer.style.display = "block";
+  function hideResultContainer() {
     resultContainer.style.display = "none";
-  } else {
-    showScore();
   }
-}
 
-function showScore() {
-  scoreElement.textContent = "Your score: " + score + "/" + questions.length;
-  scoreContainer.style.display = "block";
-}
-var viewHighScoresButton = document.getElementById("high-scores-btn");
-var highscoreContainer = document.getElementById("highscore-container");
+  function hideNextButton() {
+    nextButton.style.display = "none";
+    isNextButtonListenerAdded = false; // Reset the flag
+  }
 
-var quizActive = false; // Variable to track if the quiz is active
-var questionContainer = document.getElementById("question-container");
+  var restartButton = document.getElementById("restart-btn");
+  restartButton.addEventListener("click", restartQuiz);
 
-// Function to start the quiz
-function startQuiz() {
-  quizActive = true; // Set the quiz as active
+  var viewHighScoresButton = document.getElementById("high-scores-btn");
+  var highscoreContainer = document.getElementById("highscore-container");
 
-  // Hide the high scores container
-  highscoreContainer.style.display = "none";
-
-  // Declare questionContainer here
+  var quizActive = false;
   var questionContainer = document.getElementById("question-container");
 
-  // Hide the start button
-  var startButton = document.getElementById("start-btn");
-  console.log("startButton:", startButton);
-
-  if (!startButton) {
-    console.error("Error: Could not find the 'start-btn' element.");
-    return;
-  }
-
-  startButton.style.display = "none";
-
-}
-
-function showHighScores() {
-  if (!quizActive) { // Only allow clicking on the button if the quiz is not active
-    // Hide the quiz elements
-    questionContainer.style.display = "none";
-    resultContainer.style.display = "none";
-    scoreContainer.style.display = "none";
-
-    // Show the high scores container
-    highscoreContainer.style.display = "block";
-    highscoreContainer.classList.add("show");
-
-    // Retrieve and display the high scores
-    displayHighScores();
-  }
-}
-
-// Function to hide the high scores container
-function hideHighScores() {
-  if (!quizActive) {
-    console.log("Hiding high scores");
-    highscoreContainer.addEventListener("transitionend", function onTransitionEnd() {
-      console.log("Transition ended");
-      // Remove the event listener to prevent multiple calls
-      highscoreContainer.removeEventListener("transitionend", onTransitionEnd);
-      
-      // Show the quiz elements
-      if (!quizActive) {
-        console.log("Displaying quiz elements");
-        questionContainer.style.display = "block";
-        resultContainer.style.display = "block";
-        scoreContainer.style.display = "block";
-      }
-    });
-
-    // Hide the high scores container
+  function startQuiz() {
+    quizActive = true;
     highscoreContainer.style.display = "none";
-    highscoreContainer.classList.remove("show");
+    var questionContainer = document.getElementById("question-container");
+    var startButton = document.getElementById("start-btn");
+    startButton.style.display = "none";
+    startTimer();
+    showQuestion();
   }
-}
 
-// Adjusted event listener for the high scores button
-viewHighScoresButton.addEventListener("click", function () {
-  if (!quizActive) {
-    if (!highscoreContainer.classList.contains("show")) {
-      showHighScores();
-    } else {
-      hideHighScores();
-    }
+  function restartQuiz() {
+    quizActive = false;
+    currentQuestionIndex = 0;
+    score = 0;
+    showQuestion();
+    startTimer();
+    feedbackElement.textContent = "";
+    resultContainer.style.display = "none";
+    highscoreContainer.style.display = "none";
   }
-});
 
-var submitButton = document.getElementById("submit-button");
-var initialsInput = document.getElementById("initials-input");
+  var submitButton = document.getElementById("submit-button");
+  var initialsInput = document.getElementById("initials-input");
 
-submitButton.addEventListener("click", function(event) {
-  event.preventDefault();
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    var initials = initialsInput.value;
+    var scoreData = {
+      initials: initials,
+      score: score
+    };
 
-  var initials = initialsInput.value;
-  var scoreData = {
-    initials: initials,
-    score: score
-  };
-
-  // Retrieve existing high scores from localStorage
-  var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-
-  // Add the current score to the high scores array
-  highscores.push(scoreData);
-
-  // Save the updated high scores back to localStorage
-  localStorage.setItem("highscores", JSON.stringify(highscores));
-
-  // Display the updated high scores
-  displayHighScores();
-});
-
-function displayHighScores() {
-  var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-  var highscoreList = document.getElementById("highscore-list");
-
-  // Clear the existing high score list
-  highscoreList.innerHTML = "";
-
-  // Sort high scores in descending order
-  highscores.sort(function (a, b) {
-    return b.score - a.score;
+    var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+    highscores.push(scoreData);
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    displayHighScores();
   });
 
-  // Iterate over the high scores and create list items for each score
-  highscores.forEach(function (highscore, index) {
-    var li = document.createElement("li");
-    li.textContent = highscore.initials + ": " + highscore.score;
+  function displayHighScores() {
+    var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+    var highscoreList = document.getElementById("highscore-list");
 
-    // Add a remove button for each high score
-    var removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", function () {
-      removeHighScore(index);
+    highscoreList.innerHTML = "";
+    highscores.sort(function (a, b) {
+      return b.score - a.score;
     });
 
-    li.appendChild(removeButton);
-    highscoreList.appendChild(li);
-  });
+    highscores.forEach(function (highscore, index) {
+      var li = document.createElement("li");
+      li.textContent = highscore.initials + ": " + highscore.score;
+
+      var removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.addEventListener("click", function () {
+        removeHighScore(index);
+      });
+
+      li.appendChild(removeButton);
+      highscoreList.appendChild(li);
+    });
+
+    initialsInput.value = "";
+  }
 
   function removeHighScore(index) {
-    // Retrieve existing high scores from localStorage
     var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-  
-    // Check if the index is valid
+
     if (index >= 0 && index < highscores.length) {
-      // Remove the high score at the specified index
       highscores.splice(index, 1);
-  
-      // Save the updated high scores back to localStorage
       localStorage.setItem("highscores", JSON.stringify(highscores));
-  
-      // Display the updated high scores
       displayHighScores();
     }
   }
 
-  // Clear the initials input field
-  initialsInput.value = "";
-}
+  var viewHighScoresButton = document.getElementById("high-scores-btn");
 
-function showScore() {
-  stopTimer();
-  scoreElement.textContent = "Your score: " + score + "/" + questions.length;
-  scoreContainer.style.display = "block";
-  highscoreContainer.style.display = "block";
-}
+  viewHighScoresButton.addEventListener("click", function () {
+    if (!quizActive) {
+      if (!highscoreContainer.classList.contains("show")) {
+        showHighScores();
+      } else {
+        hideHighScores();
+      }
+    }
+  });
 
-function restartQuiz() {
-  quizActive = false; // Set the quiz as inactive
-  currentQuestionIndex = 0;
-  score = 0;
-  showQuestion(); // Show the first question
-  startTimer(); // Restart the timer
+  function showHighScores() {
+    if (!quizActive) {
+      questionContainer.style.display = "none";
+      resultContainer.style.display = "none";
+      scoreElement.style.display = "none";
+      highscoreContainer.style.display = "block";
+      highscoreContainer.classList.add("show");
+      displayHighScores();
+    }
+  }
 
-  // Clear previous feedback
-  feedbackElement.textContent = "";
+  function hideHighScores() {
+    if (!quizActive) {
+      highscoreContainer.addEventListener("transitionend", function onTransitionEnd() {
+        highscoreContainer.removeEventListener("transitionend", onTransitionEnd);
+        if (!quizActive) {
+          questionContainer.style.display = "block";
+          resultContainer.style.display = "block";
+          scoreElement.style.display = "block";
+        }
+      });
 
-  // Display or hide the appropriate containers
-  questionContainer.style.display = "block";
-  resultContainer.style.display = "none";
-  scoreContainer.style.display = "none";
-  highscoreContainer.style.display = "none";
-}
-
-restartButton.addEventListener("click", restartQuiz);
-nextButton.addEventListener("click", showNextQuestion);
-
-showQuestion();
+      highscoreContainer.style.display = "none";
+      highscoreContainer.classList.remove("show");
+    }
+  }
 
 });
